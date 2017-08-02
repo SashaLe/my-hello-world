63d009608e884de20e26525fe01219e8c8cbb433
SashaLe <30282663+SashaLe@users.noreply.github.com>
2017-08-02 10:44:24 +0300

    add eclipse project

---
+++ OldProject1/src/Package1/MainClass1.java
@@ -0,0 +1,15 @@
+package Package1;
+
+
+
+public class MainClass1 {
+	public void JustHello() {
+	//конструктор класса, вызывается из метода main
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+
+	
+}
